70c3fa51ab2846c1ca5861680d8052a400b63d53
tsubakimoto <tkmbmbr@gmail.com>
2015-05-19 01:02:48 +0900

    Libraryプロジェクトを追加

---
+++ Library/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Library
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public void Greet()
+        {
+            System.Console.WriteLine("Hello! I'm {0} and {1} years old.", this.Name, this.Age);
+        }
+    }
+}
