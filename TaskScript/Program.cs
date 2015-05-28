433cf2071d4d683e5b26b650985039a57834fc5c
tsubakimoto <tkmbmbr@gmail.com>
2015-05-28 22:06:54 +0900

    非同期処理のサンプルを別プロジェクトに移動

---
+++ TaskScript/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis.Scripting.CSharp;
+using System;
+using System.IO;
+
+namespace TaskScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // スクリプト内で非同期処理を実行する
+            Console.WriteLine("script start");
+            var code = File.ReadAllText(@"..\TaskScript\task.script.txt");
+            var script = CSharpScript.Create(code);
+            script.Run();
+            Console.WriteLine("script end");
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
